25535c4a2d34ae47b4889636566a994cd0ef63f3
DenisV <naum2891@mail.ru>
2022-09-16 02:32:21 +0300

    add "Сумма элементов массива на не чётных позициях. "

---
+++ Task5.2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit)
+    {
+        return number;
+    }
+    throw new Exception("Вы ввели не число");
+}
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int oddElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+
+    return sum;
+}
+
+const int MIN_ELEMENTS = -100;
+const int MAX_ELEMENTS = 100;
+
+int length = Prompt("Введите длину массива > ");
+int[] array = GenerateArray(length, MIN_ELEMENTS, MAX_ELEMENTS);
+PrintArray(array);
+System.Console.WriteLine($"Сумма элементов массива на нечётных местах : {oddElements(array)}");
