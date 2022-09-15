7d9c7377e0f948f9b1873175a9d45e58b9e9641c
DenisV <naum2891@mail.ru>
2022-09-16 01:49:37 +0300

    add "Чётные элементы массива. "

---
+++ Task5.1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit)
+    {
+        return number;
+    }
+    throw new Exception("Вы ввели не число");
+}
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int evenElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum += 1;
+        }
+    }
+
+    return sum;
+}
+
+const int MIN_ELEMENTS = 100;
+const int MAX_ELEMENTS = 999;
+
+int length = Prompt("Введите длину массива > ");
+int[] array = GenerateArray(length, MIN_ELEMENTS, MAX_ELEMENTS);
+PrintArray(array);
+System.Console.WriteLine($"Количество чётных чисел в массиве : {evenElements(array)}");
+
